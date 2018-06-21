08effee728086505e6cb267fac7e30ed492870e8
Suyesh Bhandari <suyeshb@gmail.com>
2018-06-21 19:49:18 -0400

    first commit. installed dependencies

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from 'react-dom';
+import './index.css';
+import App from './app/layout/App';
+import registerServiceWorker from './registerServiceWorker';
+
+render(<App/>, document.getElementById('root'));
+registerServiceWorker();
