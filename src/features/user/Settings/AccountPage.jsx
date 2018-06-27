e6b9ae0ae317f46f604d82497341356374ae0588
Suyesh Bhandari <suyeshb@gmail.com>
2018-06-27 12:51:20 -0400

    .

--- src/features/user/Settings/AccountPage.jsx
+++ src/features/user/Settings/AccountPage.jsx
@@ -1,10 +1,65 @@
-import React, { Fragment } from 'react';
+import React, {Fragment} from 'react';
+import { Segment, Header, Form, Divider, Label, Button, Icon } from 'semantic-ui-react';
+import { Field, reduxForm } from 'redux-form';
+import TextInput from '../../../app/common/form/TextInput';
 
-const AccountPage = () => (
-  <Fragment>
-    <h1>Account page</h1>
-  </Fragment>
-)
+const AccountPage = ({ error }) => {
+  return (
+    <Segment>
+      <Header dividing size="large" content="Account" />
+      <Fragment>
+        <Header color="teal" sub content="Change password" />
+        <p>Use this form to update your account settings</p>
+        <Form>
+          <Field
+            width={8}
+            name="newPassword1"
+            type="password"
+            pointing="left"
+            inline={true}
+            component={TextInput}
+            basic={true}
+            placeholder="New Password"
+          />
+          <Field
+            width={8}
+            name="newPassword2"
+            type="password"
+            inline={true}
+            basic={true}
+            pointing="left"
+            component={TextInput}
+            placeholder="Confirm Password"
+          />
+          {error && (
+            <Label basic color="red">
+              {error}
+            </Label>
+          )}
+          <Divider />
+          <Button size="large" positive content="Update Password" />
+        </Form>
+      </Fragment>
 
+      <Fragment>
+        <Header color="teal" sub content="Facebook Account" />
+        <p>Please visit Facebook to update your account settings</p>
+        <Button type="button" color="facebook">
+          <Icon name="facebook" />
+          Go to Facebook
+        </Button>
+      </Fragment>
 
-export default AccountPage;
+      <Fragment>
+        <Header color="teal" sub content="Google Account" />
+        <p>Please visit Google to update your account settings</p>
+        <Button type="button" color="google plus">
+          <Icon name="google plus" />
+          Go to Google
+        </Button>
+      </Fragment>
+    </Segment>
+  );
+};
+
+export default reduxForm({ form: 'account' })(AccountPage);
